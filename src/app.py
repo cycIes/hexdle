748f6bb35be966d2a571f87d587de170f3b125ce
CycIes <79021718+cycIes@users.noreply.github.com>
2025-11-23 12:33:52 -0500

    add input fields to guess color

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, jsonify
 from random import randrange
 
 app = Flask(__name__)
@@ -15,13 +15,33 @@ def after_request(response):
 def random_color():
     color = ""
     for i in range(3):
-        color += str(hex(randrange(0, 255)))[-2:]
-    return "#" + color
+        color += str(hex(randrange(0, 255)))[2:].zfill(2)
+    return color
 
-# color = random_color()
+color = random_color()
+attempts = []
+victory = False
 
 
 @app.route("/")
 def index():
-    color = random_color()
-    return render_template("index.html", color=color, hex=color)
+    # color = random_color()
+    return render_template("index.html", color=f"#{color}", hex=color, attempts=attempts)
+
+@app.route("/check", methods=["POST"])
+def check():
+    answer = [input for input in request.form.values()]
+    length = len(color)
+    correct = ["correct" if answer[i] == color[i] else "incorrect" for i in range(length)]
+    attempt = [{"value": answer[i], "correct": correct[i]} for i in range(length)]
+    attempts.append(attempt)
+    info = {"feedback": correct, "victory": False in correct}
+    if "incorrect" in correct:
+        return redirect("/")
+    else:
+        return redirect("/victory")
+    # return info
+    
+@app.route("/victory")
+def victory():
+    return None
